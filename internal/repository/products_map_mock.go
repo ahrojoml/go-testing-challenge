28435b58febf9cf618e69f43161605b9b3078b21
ahrojoml <andres.huerta@mercadolibre.cl>
2024-02-15 16:19:12 -0300

    chore(products): add spy

--- internal/repository/products_map_mock.go
+++ internal/repository/products_map_mock.go
@@ -8,8 +8,15 @@ func NewProductsMapMock() *ProductsMapMock {
 
 type ProductsMapMock struct {
 	SearchProductsFunc func(query internal.ProductQuery) (map[int]internal.Product, error)
+	Spy                struct {
+		SearchProductsCallCount int
+		SearchProductsArgs      []internal.ProductQuery
+	}
 }
 
 func (m *ProductsMapMock) SearchProducts(query internal.ProductQuery) (map[int]internal.Product, error) {
+	m.Spy.SearchProductsCallCount++
+	m.Spy.SearchProductsArgs = append(m.Spy.SearchProductsArgs, query)
+
 	return m.SearchProductsFunc(query)
 }
